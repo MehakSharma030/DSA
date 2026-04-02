8627ae6cd55af484d34cff53964177326330ea84
Mehak Sharma <mehaksharma0030@gmail.com>
2026-04-02 10:43:14 +0530

    Queues STL

--- QueueSTL.cpp
+++ QueueSTL.cpp
@@ -4,17 +4,29 @@ using namespace std;
 int main(){
     queue<int> q;
     q.push(10);
+    cout<<"Front element : "<<q.front()<<endl;
+    cout<<"Rear element : "<<q.back()<<endl;
+
     q.push(20);
+    cout<<"Front element : "<<q.front()<<endl;
+    cout<<"Rear element : "<<q.back()<<endl;
+
     q.push(30);
-    cout<<"size of queue is : "<<q.size()<<endl;
-    cout<<"After pushing elements "<<endl;
-    cout<<"Top element : "<<q.front()<<endl;
-    cout<<"Last element : "<<q.back()<<endl;
+    cout<<"Front element : "<<q.front()<<endl;
+    cout<<"Rear element : "<<q.back()<<endl;
+    cout<<"size of queue is : "<<q.size()<<endl;    
 
     q.pop();
-    cout<<"After deleting element top is :"<<q.front()<<endl;
-    cout<<"is the queue empty : "<<q.empty()<<endl;
-
+    q.pop();
+    cout<<"Front element:"<<q.front()<<endl;
+    cout<<"Rear element : "<<q.back()<<endl;
+    
+    if(q.empty()){
+        cout<<endl<<"Empty Queue "<<endl;
+    }
+    else{
+        cout<<endl<<"Non-Empty Queue"<<endl;
+    }
 
     return 0;
 }
